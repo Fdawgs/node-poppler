f1481cc2568b74a37aafef1d7f7f9ddda50e3819
Frazer Smith <frazer.dev@outlook.com>
2019-10-13 17:09:41 +0100

    Convert to class; add skeleton pdtohtml method

--- src/index.js
+++ src/index.js
@@ -1,34 +1,72 @@
 const os = require('os');
 const path = require('path');
+const { execFile } = require('child_process');
 
 const platform = os.platform();
-let popplerPath;
 
-// Build path to Poppler binaries based on OS
-switch (platform) {
-	// Windows OS
-	case 'win32':
-		popplerPath = path.join(
-			__dirname,
-			'lib',
-			'win',
-			'poppler-0.68.0_x86',
-			'bin'
-		);
-		console.log(popplerPath);
-		break;
+class Poppler {
+	constructor(binPath) {
+		if (binPath) {
+			this.popplerPath = binPath;
+		} else {
+			let popplerPath;
 
-	// macOS
-	case 'darwin':
-		popplerPath = __dirname;
-		break;
+			// Build path to Poppler binaries based on OS
+			switch (platform) {
+				// Windows OS
+				case 'win32':
+					popplerPath = path.join(
+						__dirname,
+						'lib',
+						'win',
+						'poppler-0.68.0_x86',
+						'bin'
+					);
+					break;
 
-	default:
-		console.error(`${platform} is NOT supported.`);
-		process.exit(1);
-		break;
+				// macOS
+				case 'darwin':
+					popplerPath = __dirname;
+					break;
+
+				default:
+					console.error(`${platform} is NOT supported.`);
+					process.exit(1);
+					break;
+			}
+
+			this.popplerPath = popplerPath;
+		}
+	}
+
+	pdfToHtml(file, options) {
+		return new Promise((resolve, reject) => {
+			const args = [];
+
+			// Build array of arguments based on options passed
+			if (options.quiet) {
+				args.push('-q');
+			}
+			if (options.version) {
+				args.push('-v');
+			}
+
+			args.push(file);
+
+			console.log(path.join(this.popplerPath, 'pdftohtml'));
+
+			execFile(path.join(this.popplerPath, 'pdftohtml'), args, (err, stdout) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(stdout);
+				}
+			});
+		});
+	}
 }
+};
 
 module.exports = {
-	popplerPath
+	Poppler
 };

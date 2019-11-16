cbcd3337d57d8fa5fac8f3c8398b0a53cd783930
Frazer Smith <frazer.dev@outlook.com>
2019-11-16 17:58:40 +0000

    Add tests for pdfToCairo function; Add clean function

--- src/index.test.js
+++ src/index.test.js
@@ -1,19 +1,27 @@
 const fs = require('fs');
 const { Poppler } = require('./index');
 
-const file = `${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitution.pdf`;
+const testDirectory = `${__dirname}/../test_docs/`;
+const file = `${testDirectory}pdf_1.3_NHS_Constitution.pdf`;
+
+function clean() {
+	if (fs.existsSync(`${testDirectory}pdf_1.3_NHS_Constitution.html`)) {
+		fs.unlinkSync(`${testDirectory}pdf_1.3_NHS_Constitution.html`);
+	}
+	if (fs.existsSync(`${testDirectory}pdf_1.3_NHS_Constitution_ind.html`)) {
+		fs.unlinkSync(`${testDirectory}pdf_1.3_NHS_Constitution_ind.html`);
+	}
+	if (fs.existsSync(`${testDirectory}pdf_1.3_NHS_Constitutions.html`)) {
+		fs.unlinkSync(`${testDirectory}pdf_1.3_NHS_Constitutions.html`);
+	}
+	if (fs.existsSync(`${testDirectory}pdf_1.3_NHS_Constitutions.svg`)) {
+		fs.unlinkSync(`${testDirectory}pdf_1.3_NHS_Constitutions.svg`);
+	}
+}
 
 describe('pdfToHtml function', () => {
-	afterEach(async () => {
-		if (fs.existsSync(`${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitution.html`)) {
-			fs.unlinkSync(`${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitution.html`);
-		}
-		if (fs.existsSync(`${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitution_ind.html`)) {
-			fs.unlinkSync(`${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitution_ind.html`);
-		}
-		if (fs.existsSync(`${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitutions.html`)) {
-			fs.unlinkSync(`${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitutions.html`);
-		}
+	afterAll(() => {
+		clean();
 	});
 
 	test('Should convert PDF file to HTML file', async () => {
@@ -22,7 +30,7 @@ describe('pdfToHtml function', () => {
 		await poppler.pdfToHtml(undefined, file)
 			.then((res) => {
 				expect(typeof res).toBe('string');
-				expect(fs.existsSync(`${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitution.html`)).toBe(true);
+				expect(fs.existsSync(`${testDirectory}pdf_1.3_NHS_Constitution.html`)).toBe(true);
 			});
 	});
 
@@ -36,13 +44,13 @@ describe('pdfToHtml function', () => {
 		await poppler.pdfToHtml(options, file)
 			.then((res) => {
 				expect(typeof res).toBe('string');
-				expect(fs.existsSync(`${__dirname}/../test_pdfs/pdf_1.3_NHS_Constitution.html`)).toBe(true);
+				expect(fs.existsSync(`${testDirectory}pdf_1.3_NHS_Constitution.html`)).toBe(true);
 			});
 	});
 
 	test('Should return an Error object if file passed not PDF format', async () => {
 		const poppler = new Poppler();
-		const testTxtFile = `${__dirname}/../test_pdfs/test.txt`;
+		const testTxtFile = `${testDirectory}test.txt`;
 
 		expect.assertions(1);
 		await poppler.pdfToHtml(undefined, testTxtFile)
@@ -85,6 +93,62 @@ describe('pdfToHtml function', () => {
 		await poppler.pdfToHtml(options, file)
 			.catch((err) => {
 				expect(err.message).toEqual('Invalid option provided \'middlePageToPrint\'');
+			});
+	});
+});
+
+describe('pdfToCairo function', () => {
+	afterAll(() => {
+		clean();
+	});
+
+	test('Should convert PDF file to SVG file', async () => {
+		const poppler = new Poppler();
+		const options = {
+			svgFile: true
+		};
+		const outPutFile = `${__dirname}/../test_docs/pdf_1.3_NHS_Constitution.svg`;
+
+		await poppler.pdfToCairo(options, file, outPutFile)
+			.then((res) => {
+				expect(typeof res).toBe('string');
+				expect(fs.existsSync(`${__dirname}/../test_docs/pdf_1.3_NHS_Constitution.svg`)).toBe(true);
+			});
+	});
+
+	test('Should return an Error object if no format option is passed to function', async () => {
+		const poppler = new Poppler();
+
+		expect.assertions(1);
+		await poppler.pdfToCairo(undefined, file)
+			.catch((err) => {
+				expect(err.message.substring(0, 15)).toBe('Command failed:');
+			});
+	});
+
+	test('Should return an Error object if invalid value types provided for an option are passed to function', async () => {
+		const poppler = new Poppler();
+		const options = {
+			pdfFile: 'test'
+		};
+
+		expect.assertions(1);
+		await poppler.pdfToCairo(options, file)
+			.catch((err) => {
+				expect(err.message).toEqual('Invalid value type provided for option \'pdfFile\', expected boolean but recieved string');
+			});
+	});
+
+	test('Should return an Error object if invalid option is passed to function', async () => {
+		const poppler = new Poppler();
+		const options = {
+			wordFile: 'test'
+		};
+
+		expect.assertions(1);
+		await poppler.pdfToCairo(options, file)
+			.catch((err) => {
+				expect(err.message).toEqual('Invalid option provided \'wordFile\'');
 			});
 	});
 });

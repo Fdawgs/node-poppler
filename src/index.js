cda680c7957fe1eeeb4581b3d58fcab3336ecf29
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-01-12 12:03:19 +0000

    Add pdftoppm function

--- src/index.js
+++ src/index.js
@@ -344,7 +344,7 @@ class Poppler {
 	 * @param {Object} options
 	 * @param {String=} options.antialias Set the cairo antialias option used for text
 	 * and drawing in image files (or rasterized regions in vector output).
-	 * Options are: default; none; gray; subpixel; fast; good; best
+	 * Options are: default; none; gray; subpixel; fast; good; best.
 	 * @param {Boolean=} options.cropBox - Uses the crop box rather than media box when
 	 * generating the files (PNG/JPEG/TIFF only).
 	 * @param {Number=} options.cropHeight - Specifies the height of crop area in pixels
@@ -604,6 +604,137 @@ class Poppler {
 
 	/**
 	 * @author Frazer Smith
+	 * @description Converts PDF to to colour image files in Portable Pixmap (PPM) format,
+	 * grayscale image files in Portable Graymap (PGM) format, or monochrome image files
+	 * in Portable Bitmap (PBM) format.
+	 *
+	 * @param {Object} options
+	 * @param {String=} options.antialiasFonts - Enable or disable font anti-aliasing.
+	 * This defaults to "yes".
+	 * @param {String=} options.antialiasVectors - Enable or disable vector anti-aliasing.
+	 * This defaults to "yes".
+	 * @param {Boolean=} options.cropBox - Uses the crop box rather than media box when
+	 * generating the files (PNG/JPEG/TIFF only).
+	 * @param {Number=} options.cropHeight - Specifies the height of crop area in pixels
+	 * (image output) or points (vector output).
+	 * @param {Number=} options.cropSize - Specifies the size of crop square in pixels
+	 * (image output) or points (vector output).
+	 * @param {Number=} options.cropWidth - Specifies the width of crop area in pixels
+	 * (image output) or points (vector output).
+	 * @param {Number=} options.cropXAxis - Specifies the x-coordinate of the crop area top left
+	 * corner in pixels (image output) or points (vector output).
+	 * @param {Number=} options.cropYAxis - Specifies the y-coordinate of the crop area top left
+	 * corner in pixels (image output) or points (vector output).
+	 * @param {Boolean=} options.evenPagesOnly - Generates only the even numbered pages.
+	 * @param {Number=} options.firstPageToConvert - Specifies the first page to convert.
+	 * @param {String=} options.freetype - Enable or disable FreeType (a TrueType / Type 1 font rasterizer).
+	 * This defaults to "yes".
+	 * @param {Boolean=} options.grayscaleFile - Generate a grayscale PGM file (instead of a color PPM file).
+	 * @param {Boolean=} options.jpegFile - Generates a JPEG file instead a PPM file.
+	 * @param {Number=} options.lastPageToConvert - Specifies the last page to convert.
+	 * @param {Boolean=} options.monochromeFile - Generate a monochrome PBM file (instead of a color PPM file).
+	 * @param {Boolean=} options.oddPagesOnly - Generates only the odd numbered pages.
+	 * @param {String=} options.ownerPassword - Specify the owner password for the PDF file.
+	 * Providing this will bypass all security restrictions.
+	 * @param {Boolean=} options.pngFile - Generates a PNG file instead a PPM file.
+	 * @param {Boolean=} options.printVersionInfo - Print copyright and version information.
+	 * @param {Boolean=} options.quiet - Don't print any messages or errors.
+	 * @param {Number=} options.resolutionXAxis - Specifies the X resolution, in pixels per inch of
+	 * image files (or rasterized regions in vector output). The default is 150 PPI.
+	 * @param {Number=} options.resolutionXYAxis - Specifies the X and Y resolution, in pixels per
+	 * inch of image files (or rasterized regions in vector output). The default is 150 PPI.
+	 * @param {Number=} options.resolutionYAxis - Specifies the Y resolution, in pixels per inch of
+	 * image files (or rasterized regions in vector output). The default is 150 PPI.
+	 * @param {Number=} options.scalePageTo - Scales the long side of each page (width for landscape
+	 * pages, height for portrait pages) to fit in scale-to pixels. The size of the short side will
+	 * be determined by the aspect ratio of the page.
+	 * @param {Number=} options.scalePageToXAxis - Scales each page horizontally to fit in scale-to-x
+	 * pixels. If scale-to-y is set to -1, the vertical size will determined by the aspect ratio of
+	 * the page.
+	 * @param {Number=} options.scalePageToYAxis - Scales each page vertically to fit in scale-to-y
+	 * pixels. If scale-to-x is set to -1, the horizontal size will determined by the aspect ratio of
+	 * the page.
+	 * @param {Boolean=} options.singleFile - Writes only the first page and does not add digits.
+	 * @param {String=} options.thinLineMode - Specifies the thin line mode. This defaults to "none".
+	 * Options are: none; solid; shape.
+	 * @param {String=} options.tiffCompression - Set TIFF compression to one of "none", "packbits",
+	 * "jpeg", "lzw", or "deflate".
+	 * @param {Boolean=} options.tiffFile - Generates a TIFF file instead a PPM file.
+	 * @param {String=} options.userPassword - Specify the user password for the PDF file.
+	 * @param {String} file - Filepath of the PDF file to read.
+	 * @param {String=} outputPath - Filepath to output the results to.
+	 * @returns {Promise}
+	 */
+	pdfToPpm(options, file, outputPath) {
+		return new Promise((resolve, reject) => {
+			const acceptedOptions = {
+				antialiasFonts: { arg: '-aa', type: 'string' },
+				antialiasVectors: { arg: '-aaVector', type: 'string' },
+				cropBox: { arg: '-cropbox', type: 'boolean' },
+				cropHeight: { arg: '-H', type: 'number' },
+				cropSize: { arg: '-sz', type: 'number' },
+				cropWidth: { arg: '-W', type: 'number' },
+				cropXAxis: { arg: '-x', type: 'number' },
+				cropYAxis: { arg: '-y', type: 'number' },
+				evenPagesOnly: { arg: '-e', type: 'boolean' },
+				firstPageToConvert: { arg: '-f', type: 'number' },
+				freetype: { arg: '-freetype', type: 'string' },
+				grayscaleFile: { arg: '-gray', type: 'boolean' },
+				jpegFile: { arg: '-jpeg', type: 'boolean' },
+				lastPageToConvert: { arg: '-l', type: 'number' },
+				monochromeFile: { arg: '-mono', type: 'boolean' },
+				oddPagesOnly: { arg: '-o', type: 'boolean' },
+				ownerPassword: { arg: '-opw', type: 'string' },
+				pngFile: { arg: '-png', type: 'boolean' },
+				printVersionInfo: { arg: '-v', type: 'boolean' },
+				quiet: { arg: '-q', type: 'boolean' },
+				resolutionXAxis: { arg: '-rx', type: 'number' },
+				resolutionXYAxis: { arg: '-r', type: 'number' },
+				resolutionYAxis: { arg: '-ry', type: 'number' },
+				scalePageTo: { arg: '-scale-to', type: 'number' },
+				scalePageToXAxis: { arg: '-scale-to-x', type: 'number' },
+				scalePageToYAxis: { arg: '-scale-to-y', type: 'number' },
+				singleFile: { arg: '-singlefile', type: 'boolean' },
+				thinLineMode: { arg: '-thinlinemode', type: 'string' },
+				tiffCompression: { arg: '-tiffcompression', type: 'string' },
+				tiffFile: { arg: '-tiff', type: 'boolean' },
+				userPassword: { arg: '-upw', type: 'string' }
+			};
+
+			// Build array of args based on options passed
+			const args = [];
+
+			/**
+			 * Check each option provided is valid and of the correct type,
+			 * before adding it to argument list.
+			 */
+			if (options) {
+				parseOptions(options, acceptedOptions, args).catch((err) => {
+					reject(err);
+				});
+			}
+
+			args.push(file);
+			if (outputPath) {
+				args.push(outputPath);
+			}
+
+			execFile(
+				path.join(this.popplerPath, 'pdftoppm'),
+				args,
+				(err, stdout) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(stdout);
+					}
+				}
+			);
+		});
+	}
+
+	/**
+	 * @author Frazer Smith
 	 * @description Converts PDF to PostScript (PS).
 	 *
 	 * @param {Object=} options
@@ -787,7 +918,7 @@ class Poppler {
 	 * @param {Number=} options.cropYAxis - Specifies the y-coordinate of the crop area top left
 	 * corner in pixels (image output) or points (vector output).
 	 * @param {String=} options.eolConvention - Sets the end-of-line convention to use for
-	 * text output: unix | dos | mac.
+	 * text output: unix; dos; mac.
 	 * @param {Number=} options.firstPageToConvert - Specifies the first page to convert.
 	 * @param {Number=} options.fixedWidthLayout - Assume fixed-pitch (or tabular) text, with the
 	 * specified character width (in points). This forces physical layout mode.
@@ -796,7 +927,7 @@ class Poppler {
 	 * @param {Number=} options.lastPageToConvert - Specifies the last page to convert.
 	 * @param {Boolean=} options.listEncodingOptions - List the available encodings.
 	 * @param {Boolean=} options.maintainLayout - Maintain (as best as possible) the original physical
-	 * layout of the text. The default is to ´undo' physical layout (columns, hyphenation, etc.) and
+	 * layout of the text. The default is to undo physical layout (columns, hyphenation, etc.) and
 	 * output the text in reading order.
 	 * @param {Boolean=} options.noDiagonalText - Discard diagonal text.
 	 * @param {Boolean=} options.noPageBreaks - Don't insert page breaks (form feed characters)

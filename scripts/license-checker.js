6eed6f7d698b7fe2a67ef2a62806217e0fa752eb
Frazer Smith <frazer.dev@outlook.com>
2023-03-15 11:50:02 +0000

    chore(scripts): throw more useful error

--- scripts/license-checker.js
+++ scripts/license-checker.js
@@ -1,3 +1,5 @@
+/* eslint-disable security-node/detect-crlf */
+/* eslint-disable security/detect-object-injection */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 const checker = require("license-checker");
@@ -13,25 +15,71 @@ const path = require("upath");
  * ensure they are not copyleft.
  * @param {object} options - Options object.
  * @param {string} options.start - Path to start checking from.
- * @returns {Promise<object>} Promise resolving to object of licenses.
  */
 async function checkLicenses(
 	options = { start: path.joinSafe(__dirname, "..") }
 ) {
 	console.log("Checking licenses of direct production dependencies...");
 
+	/**
+	 * List of deprecated copyleft license identifiers
+	 *
+	 * @see https://spdx.org/licenses/
+	 */
+	const deprecatedLicenseList = [
+		"AGPL-1.0",
+		"AGPL-3.0",
+		"GFDL-1.1",
+		"GFDL-1.2",
+		"GFDL-1.3",
+		"GPL-1.0",
+		"GPL-1.0+",
+		"GPL-2.0",
+		"GPL-2.0+",
+		"GPL-2.0-with-autoconf-exception",
+		"GPL-2.0-with-bison-exception",
+		"GPL-2.0-with-classpath-exception",
+		"GPL-2.0-with-font-exception",
+		"GPL-2.0-with-GCC-exception",
+		"GPL-3.0",
+		"GPL-3.0+",
+		"GPL-3.0-with-autoconf-exception",
+		"GPL-3.0-with-GCC-exception",
+		"LGPL-2.0",
+		"LGPL-2.0+",
+		"LGPL-2.1",
+		"LGPL-2.1+",
+		"LGPL-3.0",
+		"LGPL-3.0+",
+	];
+
+	// merge copyleft licenses with deprecated licenses list
+	copyLeftLicenses.push(...deprecatedLicenseList);
+
 	const licenses = await init({
 		direct: true,
-		failOn: copyLeftLicenses.join(";"),
 		production: true,
 		start: options.start,
 	});
 
-	return licenses;
+	const copyLeftLicensesList = Object.keys(licenses).filter((license) =>
+		copyLeftLicenses.includes(licenses[license].licenses)
+	);
+
+	if (copyLeftLicensesList.length > 0) {
+		console.log(
+			`The following dependencies are using copyleft licenses: ${copyLeftLicensesList.join(
+				", "
+			)}`
+		);
+		process.exit(1);
+	}
+
+	console.log("No copyleft licenses found.");
+	process.exit(0);
 }
 
 // If file called directly, then run function
-/* istanbul ignore if */
 if (require.main === module) {
 	checkLicenses();
 }

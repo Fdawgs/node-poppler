91a11f53a5592f7c584a36c3dfbcf2fa602d6f5b
Frazer Smith <frazer.dev@outlook.com>
2023-09-19 19:41:43 +0100

    chore(tsconfig): disable strict mode

--- src/index.js
+++ src/index.js
@@ -36,45 +36,37 @@ function parseOptions(acceptedOptions, options, version) {
 	const invalidArgs = [];
 	Object.keys(options).forEach((key) => {
 		if (Object.hasOwn(acceptedOptions, key)) {
-			// @ts-ignore
 			// eslint-disable-next-line valid-typeof
 			if (typeof options[key] === acceptedOptions[key].type) {
 				// Skip boolean options if false
-				// @ts-ignore
+
 				if (acceptedOptions[key].type === "boolean" && !options[key]) {
 					return;
 				}
 				// Arg will be empty for some non-standard options
-				// @ts-ignore
+
 				if (acceptedOptions[key].arg !== "") {
-					// @ts-ignore
 					args.push(acceptedOptions[key].arg);
 				}
 
-				// @ts-ignore
 				if (typeof options[key] !== "boolean") {
-					// @ts-ignore
 					args.push(options[key]);
 				}
 			} else {
 				invalidArgs.push(
 					`Invalid value type provided for option '${key}', expected ${
-						// @ts-ignore
 						acceptedOptions[key].type
-						// @ts-ignore
 					} but received ${typeof options[key]}`
 				);
 			}
 
 			if (
-				// @ts-ignore
 				acceptedOptions[key].minVersion &&
 				version &&
 				// @ts-ignore: type checking is done above
 				lt(version, acceptedOptions[key].minVersion, { loose: true })
 			) {
 				invalidArgs.push(
-					// @ts-ignore
 					`Invalid option provided for the current version of the binary used. '${key}' was introduced in v${acceptedOptions[key].minVersion}, but received v${version}`
 				);
 			}

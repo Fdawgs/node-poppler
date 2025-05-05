88ce101f9a295897982859039e98d48481904043
Frazer Smith <frazer.dev@icloud.com>
2025-05-05 17:26:58 +0100

    perf(index): replace `forEach` with `for` loop in `pdfInfo`

--- src/index.js
+++ src/index.js
@@ -558,13 +558,16 @@ class Poppler {
 						 */
 						if (options.printAsJson === true) {
 							const info = {};
-							stdOut.split("\n").forEach((line) => {
+							const stdOutLines = stdOut.split("\n");
+							const stdOutLinesLength = stdOutLines.length;
+							for (let i = 0; i < stdOutLinesLength; i += 1) {
+								const line = stdOutLines[i];
 								const lines = line.split(": ");
 								if (lines.length > 1) {
 									// @ts-ignore: creating dynamic object keys
 									info[camelCase(lines[0])] = lines[1].trim();
 								}
-							});
+							}
 							resolve(info);
 						} else {
 							resolve(stdOut.trim());

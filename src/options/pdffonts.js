612d29043fed4ce688b81f9865d4909312a0271b
Frazer Smith <frazer.dev@icloud.com>
2026-05-13 14:46:54 +0100

    refactor(options): remove unknown min versions

--- src/options/pdffonts.js
+++ src/options/pdffonts.js
@@ -13,10 +13,10 @@
 
 /** @type {Record<keyof PdfFontsOptions, import("../index").OptionDetails>} */
 module.exports = {
-	firstPageToExamine: { arg: "-f", type: "number", minVersion: undefined },
-	lastPageToExamine: { arg: "-l", type: "number", minVersion: undefined },
+	firstPageToExamine: { arg: "-f", type: "number" },
+	lastPageToExamine: { arg: "-l", type: "number" },
 	listSubstitutes: { arg: "-subst", type: "boolean", minVersion: "0.19.0" },
-	ownerPassword: { arg: "-opw", type: "string", minVersion: undefined },
-	printVersionInfo: { arg: "-v", type: "boolean", minVersion: undefined },
-	userPassword: { arg: "-upw", type: "string", minVersion: undefined },
+	ownerPassword: { arg: "-opw", type: "string" },
+	printVersionInfo: { arg: "-v", type: "boolean" },
+	userPassword: { arg: "-upw", type: "string" },
 };
